8a92b9c6a00fdf2803683f2dc6859ce7e2acc001
Limberg Bernal Nuñez <limberg.biny15@gmail.com>
2024-08-20 00:34:40 -0400

    update de curso
    actualizacion

--- src/app/pages/pages.module.ts
+++ src/app/pages/pages.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from "@angular/router";
+import { FormsModule } from "@angular/forms";
 
 
 import { DashboardComponent } from './dashboard/dashboard.component';
@@ -27,8 +28,9 @@ import { SharedModule } from '../shared/shared.module';
   ],
   imports: [
     CommonModule,
+    FormsModule,
     SharedModule,
-    RouterModule
+    RouterModule,
   ]
 })
 export class PagesModule { }
--- src/app/pages/progress/progress.component.ts
+++ src/app/pages/progress/progress.component.ts
@@ -3,8 +3,22 @@ import { Component, Input } from '@angular/core';
 @Component({
   selector: 'app-progress',
   templateUrl: './progress.component.html',
-  styleUrls: ['./progress.component.css']
+  styleUrls: ['./progress.component.css'],
 })
 export class ProgressComponent {
-  @Input() valor = 0;
+  @Input() progreso = 80;
+
+  get getPorcentaje() {
+    return `${this.progreso}%`;
+  }
+
+  cambiarValor(valor: number) {
+    if(this.progreso > 100){
+      this.progreso =100;
+    }
+    if(this.progreso < 0){
+      this.progreso = 0;
+    }
+    this.progreso = this.progreso + valor;
+  }
 }
